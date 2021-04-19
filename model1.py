a280004fde8a2433a9b1f40e9e2881236b1ce1c1
JavierEA1 <jesteban97@gmail.com>
2021-04-19 21:22:24 +0200

    3

--- model1.py
+++ model1.py
@@ -4,6 +4,7 @@ m=model('model1')
 n=50
 w=6
 p=5
+sm=600
 mu=20
 pary=mu-w
 distr=np.repeat(0.1,11)
@@ -23,9 +24,16 @@ rest_coeff=np.ones((long_l,2))
 for i in range(0,long_l):
     rest_coeff[i,1]=i*p
 y=m.addVars(binvar,name="y",vtype=GRB.BINARY)
-model.addConstrs(quicksum(Const_coeff[i][j]*distr[j] for j in distr )+pary*y[i] )<=mu for i in y )
-model.addConstrs(quicksum(y[i]))
-min(t)
+
+
+m.addConstrs(quicksum(distr[i]*mu_coeff[i] for i in mu_coeff)=mu)
+m.addConstrs(quicksum(distr[i]*so_coeff[i] for i in so_coeff)=sm)
+m.addConstrs(quicksum(Const_coeff[i][j]*distr[j] for j in distr )+pary*y[i] <=mu for i in y )
+m.addConstrs(quicksum(y)=1)
+m.addConstrs(obj-5*(i)*y[i]>0 for i in y)
+m.setObjective(obj, GRB.MINIMIZE)
+m.optimize()
+
 
 
 
